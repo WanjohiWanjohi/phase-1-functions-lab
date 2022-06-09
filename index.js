da967442bf73af0db1c40d1797e67f8d33fec60e
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-09 22:10:30 +0300

    commit tree 5ebd2dbc071a5b8158fa6451216a8b5be0e476b3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distanceLength) {
+    return Math.abs(distanceLength - 42);
+}
+function distanceFromHqInFeet(distanceLength) {
+    return Math.abs(distanceLength - 42) * 264;
+}
+function distanceTravelledInFeet(startPoint, endPoint) {
+    return Math.abs(endPoint - startPoint) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = destination - start;
+    if (distance <= 1) {
+        return 0;
+    } else if (distance > 2000 && distance <2500) {
+        return 25;
+    }else if (distance > 2500 ){
+        return 0;
+    }
+}
